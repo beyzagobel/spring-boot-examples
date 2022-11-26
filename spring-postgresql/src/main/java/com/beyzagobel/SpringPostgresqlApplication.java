001251caee267cc0adca157f104532e1e0afcc3d
beyza <byzgbll@gmail.com>
2022-11-26 23:52:07 +0300

    init

--- spring-postgresql/src/main/java/com/beyzagobel/SpringPostgresqlApplication.java
+++ spring-postgresql/src/main/java/com/beyzagobel/SpringPostgresqlApplication.java
@@ -1,6 +1,10 @@
 package com.beyzagobel;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -13,10 +17,21 @@ public class SpringPostgresqlApplication {
     }
 
 
-    // kjabsfjbsak
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
+    }
+
+    // Swagger Bean
+    @Bean
+    public OpenAPI customopenAPI(@Value("${application-description}") String description,
+                                 @Value("${application-version}") String version){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Spring Examples API")
+                        .version(version)
+                        .description(description)
+                        .license(new License().name("Spring Example API Lisence")));
     }
 
 }
